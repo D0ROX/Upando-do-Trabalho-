befd93ea5c0c46671f8a0ecad59d0c5a73793679
Danniel Eduardo Dorox <ddorox@curitiba.prefs>
2025-11-07 11:45:18 -0300

    aaaaaaaaaaaaaaaa

--- src/T06/T6/TestaSomaAreas.java
+++ src/T06/T6/TestaSomaAreas.java
@@ -1,6 +1,5 @@
 package T06.T6;
 
-// Testa a soma das áreas
 public class TestaSomaAreas {
 
     public static void main(String[] args) {
@@ -9,15 +8,15 @@ public class TestaSomaAreas {
         Superficie[] formas = new Superficie[3];
 
         // Instâncias
-        formas[0] = new Retangulo(10.0, 5.0);     // 50.00
-        formas[1] = new Triangulo(10.0, 5.0);     // 25.00
-        formas[2] = new Circunferencia(5.0);      // 78.54 aprox.
+        formas[0] = new Retangulo(10.0, 5.0);
+        formas[1] = new Triangulo(10.0, 5.0);
+        formas[2] = new Circunferencia(5.0);
 
         // Soma total
         double total = CalculadorDeArea.somarAreas(formas);
 
         // Saída
-        System.out.println("--- Soma das Áreas ---");
+        System.out.println("Soma das Áreas");
         System.out.printf("Área do Retângulo: %.2f%n", formas[0].area());
         System.out.printf("Área do Triângulo: %.2f%n", formas[1].area());
         System.out.printf("Área da Circunferência: %.2f%n", formas[2].area());
