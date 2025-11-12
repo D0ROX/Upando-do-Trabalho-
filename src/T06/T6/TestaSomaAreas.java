5e99d38c7d0e537b6d0321452fc0eb1304ef2b97
Danniel Eduardo Dorox <ddorox@curitiba.prefs>
2025-11-12 11:32:10 -0300

    aaaaaaaa

--- src/T06/T6/TestaSomaAreas.java
+++ src/T06/T6/TestaSomaAreas.java
@@ -15,7 +15,6 @@ public class TestaSomaAreas {
         // Soma total
         double total = CalculadorDeArea.somarAreas(formas);
 
-        // Saída
         System.out.println("Soma das Áreas");
         System.out.printf("Área do Retângulo: %.2f%n", formas[0].area());
         System.out.printf("Área do Triângulo: %.2f%n", formas[1].area());
